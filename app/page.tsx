2d785c1bcbc88ae243162e795dc4abb1418343dc
Justin <alexanderrr416@gmail.com>
2024-04-08 21:36:41 -0400

    style: hide logo on mobile screens

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="h-screen flex flex-col">
       <div className="h-[4rem] flex md:h-[5rem] lg:h-[6rem] border-b-[1px] border-zinc-600 ">
-        <h1 className="md:w-[17rem] lg:w-[19rem]">LOGO</h1>
+        <h1 className="md:w-[17rem] lg:w-[19rem] hidden md:block">LOGO</h1>
         <MenuBar />
       </div>
       <UserContent />
