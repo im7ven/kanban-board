5cbaa83678bd32f3bf0e548f804459a71f68d2f5
Justin <alexanderrr416@gmail.com>
2024-04-27 15:25:59 -0400

    chore: set first task board to active by default

--- app/hooks/useTaskBoards.ts
+++ app/hooks/useTaskBoards.ts
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
+import useActiveTaskBorad from "../zustand/store";
 
 interface TaskBoard extends PrismaTaskBoard {
   columns: Column[];
@@ -10,6 +11,7 @@ interface TaskBoard extends PrismaTaskBoard {
 
 const useTaskBoards = () => {
   const { status } = useSession();
+  const { DefaultBoard } = useActiveTaskBorad();
   const [authenticated, setAuthenticated] = useState<boolean | null>(null);
   useEffect(() => {
     if (status === "authenticated") {
@@ -38,6 +40,12 @@ const useTaskBoards = () => {
   });
 
   const { isLoading, data: taskBoards, isError } = taskBoardQueries;
+
+  useEffect(() => {
+    if (!isLoading && taskBoards && taskBoards.length > 0) {
+      DefaultBoard(taskBoards);
+    }
+  }, [isLoading, taskBoards, DefaultBoard]);
 
   return { isLoading, isError, taskBoards, authenticated };
 };
