e6020b9e2af84c952d5e42ad7a3004531b2b1dee
Justin <alexanderrr416@gmail.com>
2024-04-29 20:21:15 -0400

    style: adjust menu bar height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import UserContent from "./components/UserContent";
 export default function Home() {
   return (
     <main className="h-screen flex flex-col">
-      <div className="h-[4rem] flex md:h-[5rem] lg:h-[6rem] border-b-[1px] border-zinc-600 ">
+      <div className="min-h-[4rem] flex md:-min-h-[5rem] lg:h-[6rem] border-b-[1px] border-zinc-600 ">
         <h1 className="md:w-[17rem] lg:w-[19rem] hidden md:block">LOGO</h1>
         <MenuBar />
       </div>
