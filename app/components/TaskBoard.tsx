f2e17ad84506fc195a20a367e8a1c35e5a1e35e8
Justin <alexanderrr416@gmail.com>
2024-04-29 20:20:42 -0400

    feat: enable vertical and horizontal scroll on task board

--- app/components/TaskBoard.tsx
+++ app/components/TaskBoard.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import useTaskBoards from "../hooks/useTaskBoards";
+import useActiveTaskBorad from "../zustand/store";
 
 const TaskBoard = ({ sidebar }: { sidebar: boolean }) => {
+  const { taskBoards } = useTaskBoards();
+  const { activeBoard } = useActiveTaskBorad();
+
+  const selectedBoard = taskBoards?.find((board) => board === activeBoard);
   return (
     <div
-      className={`md:border-l-[1px] border-zinc-600  ${
+      className={` gap-6 flex md:border-l border-zinc-600 p-4 overflow-x-auto boardContainerHeight  ${
         sidebar ? "" : "col-span-2"
       }`}
     >
-      <h2>Task Boards</h2>
+      {selectedBoard?.columns.map((col, index) => (
+        <div key={index} className="">
+          <h2 className="relative">{col.title}</h2>
+
+          <div className="w-[17.5rem]   space-y-5">
+            <div className="bg-white h-[8rem] rounded text-center ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure
+              unde magnam et. Optio, totam.
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
