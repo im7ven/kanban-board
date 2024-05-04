a3e36b8f46a214f5b3a5a9a25203b8e7cc963dbf
Justin <alexanderrr416@gmail.com>
2024-05-03 22:44:45 -0400

    chore: Move custom types into separate file

---
+++ app/types.ts
@@ -0,0 +1,18 @@
+import {
+  Column as PrismaColumn,
+  Task as PrismaTask,
+  TaskBoard as PrismaTaskBoard,
+  Subtask,
+} from "@prisma/client";
+
+export interface TaskBoard extends PrismaTaskBoard {
+  columns: Column[];
+}
+
+export interface Column extends PrismaColumn {
+  tasks: Task[];
+}
+
+export interface Task extends PrismaTask {
+  subtasks: Subtask[];
+}
