7aa021206bdad67eababd89b8de6a4c224824584
Justin <alexanderrr416@gmail.com>
2024-05-12 21:38:35 -0400

    chore: update prisma models

---
+++ prisma/migrations/20240512182244_add_is_new_prop_to_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `column` ADD COLUMN `isNew` VARCHAR(191) NULL;
---
+++ prisma/migrations/20240513002405_remove_the_is_new_prop/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isNew` on the `column` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `column` DROP COLUMN `isNew`;
