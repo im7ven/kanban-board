de94991bb68eccd799237c583d76bea92e0874af
Justin <alexanderrr416@gmail.com>
2024-05-12 21:41:01 -0400

    chore: build route handler for editing task boards

--- app/api/task-boards/[id]/route.ts
+++ app/api/task-boards/[id]/route.ts
@@ -1,4 +1,6 @@
+import { updateTaskBoardSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
@@ -21,4 +23,64 @@ export async function DELETE(
   });
 
   return NextResponse.json({});
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const validation = updateTaskBoardSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const taskBoardId = parseInt(params.id);
+
+  try {
+    const existingTaskBoardState = await prisma.taskBoard.findUnique({
+      where: { id: taskBoardId },
+      include: { columns: true },
+    });
+
+    const existingColumnIds = existingTaskBoardState?.columns.map(
+      (col) => col.id
+    );
+    const newColumnIds = validation.data.columns.map((col) => col.id);
+    const columnsToDelete = existingColumnIds?.filter(
+      (id) => !newColumnIds.includes(id)
+    );
+
+    await prisma.column.deleteMany({
+      where: { id: { in: columnsToDelete } },
+    });
+
+    await prisma.taskBoard.update({
+      where: { id: taskBoardId },
+      data: {
+        title: validation.data.title, // Update task board title
+        columns: {
+          // Update existing columns' titles
+          updateMany: validation.data.columns
+            .filter((column) => column.id !== undefined)
+            .map((column) => ({
+              where: { id: column.id },
+              data: { title: column.title },
+            })),
+          // Create new columns
+          create: validation.data.columns
+            .filter((column) => column.id === undefined)
+            .map((column) => ({
+              title: column.title,
+            })),
+        },
+      },
+    });
+
+    return NextResponse.json({});
+  } catch (error) {
+    console.error("Error updating task board:", error);
+    return NextResponse.json("Failed to update task board", { status: 500 });
+  }
 }
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -34,3 +34,20 @@ export const createTaskSchema = z.object({
     )
     .optional(),
 });
+
+export const updateTaskBoardSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required.")
+    .max(25, "Maximum of 25 characters."),
+  columns: z.array(
+    z.object({
+      id: z.number().optional(), // Optional id for new columns
+      title: z
+        .string()
+        .min(1, "Title is required when assigning columns to boards.")
+        .max(15, "Maximum of 15 characters"),
+    })
+  ),
+  isNew: z.boolean().optional(),
+});
