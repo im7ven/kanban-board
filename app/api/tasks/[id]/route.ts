a1b6cc1ad22a7c44430c4dbe72dcae6429f99c3d
Justin <alexanderrr416@gmail.com>
2024-05-27 10:36:01 -0400

    chore: build api for deleting tasks

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,5 +1,6 @@
 import { updateTaskSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -61,4 +62,23 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedTask);
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const existingTask = await prisma.task.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!existingTask) {
+    return NextResponse.json({ error: "Invalid task" }, { status: 404 });
+  }
+
+  await prisma.task.delete({
+    where: { id: existingTask.id },
+  });
+
+  return NextResponse.json({});
 }
