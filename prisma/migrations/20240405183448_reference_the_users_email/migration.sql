3d68d386a0651652e6f478288d73d3293960a4f6
Justin <alexanderrr416@gmail.com>
2024-06-02 01:14:17 -0400

    chore: update migration

--- prisma/migrations/20240405183448_reference_the_users_email/migration.sql
+++ prisma/migrations/20240405183448_reference_the_users_email/migration.sql
@@ -1,5 +1,2 @@
--- DropForeignKey
-ALTER TABLE `taskboard` DROP FOREIGN KEY `TaskBoard_created_by_fkey`;
-
 -- AddForeignKey
 ALTER TABLE `TaskBoard` ADD CONSTRAINT `TaskBoard_created_by_fkey` FOREIGN KEY (`created_by`) REFERENCES `users`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
